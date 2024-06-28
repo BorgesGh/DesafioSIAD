78b2e4994345f81ee303ec8bbac70e899af7f3d4
BorgesGh <ghabrielbc@gmail.com>
2024-06-28 10:21:07 -0300

    Adicionar arquivos auxiliares

---
+++ src/test/java/org/pessoal/desafiosiad/DesafioSiadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pessoal.desafiosiad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSiadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
