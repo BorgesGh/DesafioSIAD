b8f4d1aa04034e4163e8ccf2117525c5fc143b11
BorgesGh <ghabrielbc@gmail.com>
2024-06-28 10:20:11 -0300

    Adicionar PjController

---
+++ src/main/java/org/pessoal/desafiosiad/controller/PessoaJuridicaController.java
@@ -0,0 +1,31 @@
+package org.pessoal.desafiosiad.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.pessoal.desafiosiad.model.PessoaJuridica;
+import org.pessoal.desafiosiad.service.PessoaJuridicaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/PessoaJuridica")
+public class PessoaJuridicaController {
+
+    private PessoaJuridicaService pjService;
+
+    public PessoaJuridicaController(PessoaJuridicaService pjService) {
+        this.pjService = pjService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity savePessoaJuridica(@RequestBody PessoaJuridica pj) {
+        try {
+            pjService.save(pj);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }catch (ConstraintViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+}
