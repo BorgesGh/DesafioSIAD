760655a0410ff30e3434e32d4d0d52613ce3dfc7
BorgesGh <ghabrielbc@gmail.com>
2024-06-29 16:07:02 -0300

    Adicionar Not Found Exeception

---
+++ src/main/java/org/pessoal/desafiosiad/exceptions/NotFindException.java
@@ -0,0 +1,11 @@
+package org.pessoal.desafiosiad.exceptions;
+
+public class NotFindException extends Exception {
+    public NotFindException() {
+
+    }
+    public NotFindException(String message) {
+        super(message);
+    }
+
+}
